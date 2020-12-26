7d0ebed5d7ce73dcc3cfb5737dfb3c6d0f6da51e
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-26 15:19:05 +0100

    Initial Commit

---
+++ MovieAPI/MovieAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAPIApp.swift
+//  MovieAPI
+//
+//  Created by Andrej Kling on 26.12.20.
+//
+
+import SwiftUI
+
+@main
+struct MovieAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
